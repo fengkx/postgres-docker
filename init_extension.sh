42c163e7a17edda8ee37d4ce9e532589e6cd8d3a
fengkx <liangkx8237@gmail.com>
2021-03-13 00:07:13 +0800

    fix: no load safeupdate by default

--- init_extension.sh
+++ init_extension.sh
@@ -1,5 +1,5 @@
 set -ex
-echo "shared_preload_libraries = 'pg_stat_statements, safeupdate'" >> $PGDATA/postgresql.conf
+echo "shared_preload_libraries = 'pg_stat_statements'" >> $PGDATA/postgresql.conf
 
 echo "pg_stat_statements.max = 10000" >> $PGDATA/postgresql.conf
 echo "pg_stat_statements.track = all" >> $PGDATA/postgresql.conf
